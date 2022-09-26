c650552309b2ddfc16eb08b8bcd9b51ead541bf6
Brisco Arechederra <brisco.arch@gmail.com>
2022-09-26 10:57:22 -0600

    resume and portfolio pages created

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,114 @@
 import React from 'react'
+//import images from the back end
+import customHomepage from '../../backAssets/custom-homepage.png'
+import codeQuiz from '../../backAssets/codeQuiz.jpg'
+import noteApp from '../../backAssets/noteApp.png'
+import cacheFood from '../../backAssets/cacheFood.png'
+import readMe from '../../backAssets/readMeGen.png'
+import empSQL from '../../backAssets/empSQL.png'
 
 export default function Portfolio(){
+    const projectList = [
+        {
+            title: "Custom Home Page",
+            image: customHomepage,
+            repoLink: "https://github.com/brisco13/custom-homepage",
+            liveLink: "https://brisco13.github.io/custom-homepage/",
+            desc: "A collaborative project to create a single page application that uses HTML, JavaScript, and CSS and allows for a user to make a customized home page for their internet browser.",
+            tags: "HTML, Javascript, Third-Party APIs, CSS, Materialize",
+            num: "1"
+        },
+        {
+            title: "Cache Food Decisions",
+            image: cacheFood,
+            repoLink: "https://github.com/brisco13/cache-food-decisions",
+            liveLink: "http://damp-harbor-49836.herokuapp.com/",
+            desc: "A collaborative project that uses MySQL, Express.js, Node.js, HTML, CSS, and Javascript. This application allows the user to generate meal plans based of dietary preferences and will output a compiled shopping list with the option to send the selected recipes and shopping list to the users email. The application requires the user to register and leverages authentication to save preferences. This project is still under development and is not meeting 100% user acceptance criteria",
+            tags: "MySQL, Node.js, Express.js, CSS, Third-Party APIs, REST API, Bootstrap, Javascript",
+            num: "2"
+        },{
+            title: "Read Me Generator",
+            image: readMe,
+            repoLink: "https://github.com/brisco13/readMeGenerator",
+            liveLink: "",
+            desc: "This is a terminal operated application using Node.js to generate a polished README.md file template. Due to this being a terminal operated application, there is no deployed link.",
+            tags: "Node.js",
+            num: "3"
+        },{
+            title: "Coding Quiz",
+            image: codeQuiz,
+            repoLink: "https://github.com/brisco13/CodingQuiz",
+            liveLink: "https://brisco13.github.io/CodingQuiz/",
+            desc: "This is a quiz application that utilizes HTML, CSS, and Javascript and quizzes the user on an assortment of Javascript questions.",
+            tags: "HTML, JavaScript, CSS, Bootstrap",
+            num: "4"
+        },{
+            title: "Employee SQL Database",
+            image: empSQL,
+            repoLink: "https://github.com/brisco13/EmployeeSQLDatabase",
+            liveLink: "",
+            desc: "A Node.js and MySQL application that allows for a simple employee management system run from the terminal. Allows the tracking of employee wage, position, department, and reporting structure. Due to this being a terminal operated application, there is no deployed link.",
+            tags: "MySQL, Node.js, Express.js",
+            num: "5"
+        },{
+            title: "Note App",
+            image: noteApp,
+            repoLink: "https://github.com/brisco13/NoteApp",
+            liveLink: "https://desolate-sands-63045.herokuapp.com/",
+            desc: "This is an application that allows notes to be drafted and saved to a JSON database within the repo. This is built using Express.js, HTML, CSS, and Javascript.",
+            tags: "Express.js, Node.js, HTML, CSS, Bootstrap, Javascript",
+            num: "6"
+        },
+    ];
+
+    // create card element
+    const Card = (props) => {
+        
+        return (
+            <div class="card text-center">
+                <div class="card-header">Project: #{props.num}</div>
+                <img src={props.image} class="card-img-top" alt= {props.title}/>
+                <div class="card-body">
+                    <h5 class="card-title">{props.title}</h5>
+                    <p class="card-text">{props.desc}</p>
+                    <div class="row justify-content-evenly">
+            
+                        <a href={props.liveLink} class="col-3 btn btn-primary">See It Deployed!</a>
+                    <a href={props.repoLink} class="col-3 btn btn-primary">See the Repo!</a>
+                    </div>
+                    
+                </div>
+                <div class="card-footer text-muted">
+                    <b>Tags:</b> {props.tags}
+                </div>
+            </div>
+        )
+    }
+
     return (
     <div>
-        <h1>This is a Portfolio Section!</h1>
+        <br></br>
+        <h1 class="text-center">Portfolio</h1>
+        <br></br>
+        <div class="col justify-content-evenly">
+            
+            {projectList.map((project) => (
+                <div class="container">
+                <Card 
+                title = {project.title} 
+                image = {project.image}
+                repoLink = {project.repoLink}
+                liveLink = {project.liveLink}
+                desc = {project.desc}
+                tags = {project.tags}
+                num = {project.num}
+                />
+                <br></br>
+                <br></br>
+                </div>
+            ))}
+
+        </div>
+
     </div>)
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,82 @@ import React from 'react'
 
 export default function Resume(){
     return (
-    <div>
-        <h1>This is a Resume Section!</h1>
-    </div>)
-}
+    <div class="row justify-content-evenly">
+        <div class="container col-10">
+        <div class="res-box container">
+            <h1 class="text-center">Brisco Arechederra Resume</h1>
+            <p class= "text-center"><a  href="./assets/images/brisco-resume.pdf" download>
+                <n></n>Prefer to download a copy? Click me!<br></br></a></p>
+            <div class="row justify-content-evenly">
+                <div class="col container">
+                <p class= "res-contact text-center"><b>Cell:</b> 303-253-4461 </p>
+                </div>
+                <div class="col container">
+                <p class= "res-contact text-center"><b>Email:</b> brisco.arch@gmail.com </p>
+                </div>
+                <div class="col container">
+                <p class= "res-contact text-center"><b>Location:</b> Longmont, CO</p>
+                </div>
+                <div class="col container">
+                <p class= "res-contact text-center"><b>GitHub:</b> Brisco13</p>
+                </div>
+            </div>
+        </div>
+        
+        <br></br>
+        <h4>Work Experience:</h4>
+        <hr class="section"></hr>
+        <h5 class="split-para">FWI | Poppulo <span>Sep 2022 - Current</span></h5>
+        <h6>System Engineer I</h6>
+        <ul>
+            <li>Job Role 1</li>
+            <li>Job Role 2</li>
+            <li>Job Role 3</li>
+            <li>Job Role 4</li>
+        </ul>
+        <hr class="dashed"></hr>
+        <h5 class="split-para">AGC Biologics <span>Aug 2021 - Aug 2022</span></h5>
+        <h6>Quality Control Associate III</h6>
+        <ul>
+            <li>Job Role 1</li>
+            <li>Job Role 2</li>
+            <li>Job Role 3</li>
+            <li>Job Role 4</li>
+        </ul>
+
+        <br></br>
+        <h4>Education:</h4>
+        <hr class="section"></hr>
+        <h5 class="split-para">Web Development Coding Bootcamp <span>Apr 2022 - Oct 2022</span></h5>
+        <h6>Denver University - Denver, CO</h6>
+        <br></br>
+        <h5 class="split-para">Bachelor of Science with Honors, Chemical and Biological Engineering<span>Aug 2013 - May 2017</span></h5>
+        <h6 class="split-para">Colorado State University - Fort Collins, CO<span><b>GPA:</b> 3.62</span></h6>
+
+        <br></br>
+        <h4>Profession Knowledge and Skills:</h4>
+        <hr class="section"></hr>
+        <h5><b>Web Development:</b></h5>
+        <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>Third-Party APIS, REST APIs </li>
+            <li>Node.js, Express.js</li>
+            <li>SQL, mySQL, Sequelize, MongoDB, Mongoose, NoSQL</li>
+            <li>Agile Development</li>
+            <li>React</li>
+        </ul>
+        <h5><b>Web Development:</b></h5>
+        <ul>
+            <li>Object-Oriented Programming</li>
+            <li>JAVA, Python, Matlab</li>
+            <li>Microsoft Office</li>
+            <li>Microsoft Power Automate and Forms</li>
+            <li>Electronic Lab Notebooks</li>
+        </ul>
+
+
+
+        </div>
+    </div>
+    
+    )}
